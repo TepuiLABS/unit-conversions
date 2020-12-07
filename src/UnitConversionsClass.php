91832ff29efcb6fe92ea4f441eaf96d48a1a747a
angel cruz <angel.c@geopagos.com>
2020-12-07 17:49:25 -0300

    convierte de kilos a lbs :rocket:

--- src/UnitConversionsClass.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Tepuilabs\UnitConversions;
-
-class UnitConversionsClass
-{
-}
---
+++ tests/WeightTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tepuilabs\UnitConversions\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Tepuilabs\UnitConversions\Weight;
+
+class WeightTest extends TestCase
+{
+    /** @test */
+    public function it_can_conver_kilograms_to_lbs()
+    {
+		$lbs = Weight::fromKilograms(100)->toLbs();
+
+		$this->assertEquals(220.4623, $lbs);
+    }
+}
